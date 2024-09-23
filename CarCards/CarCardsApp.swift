f6b7f283acb31d2dad410c16ad9196654883f6b3
Casaquinho <marcelohmanzo@gmail.com>
2024-09-23 15:15:08 -0300

    Initial Commit

---
+++ CarCards/CarCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCardsApp.swift
+//  CarCards
+//
+//  Created by MARCELO HENRRIQUE MORELLO MANZO on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CarCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
