9ace6c7d255c4ea3dd3e30ff881863ae6c5cd3ea
Casaquinho <marcelohmanzo@gmail.com>
2024-11-29 15:31:08 -0300

    Add car images and update ContentView.swift

--- CarCards/ContentView.swift
+++ CarCards/ContentView.swift
@@ -2,23 +2,7 @@
 //  ContentView.swift
 //  CarCards
 //
-//  Created by MARCELO HENRRIQUE MORELLO MANZO on 23/09/24.
+//  Created by MARCELO HENRRIQUE MORELLO MANZO on 02/10/24.
 //
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    ContentView()
-}
+import Foundation
